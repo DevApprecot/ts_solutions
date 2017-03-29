ecf9e170e2e36e073d1d8564d9cec5f68782b75a
giorgosTh <gt@anaxoft.com>
2017-03-29 14:38:52 +0300

    ApprecotRestService

---
+++ Ts Solutions/Ts Solutions/Ts_Solutions/Network/ApprecotRestService.cs
@@ -0,0 +1,55 @@
+﻿using ModernHttpClient;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Ts_Solutions.Network
+{
+    public class ApprecotRestService
+    {
+        public static readonly HttpClient Client = new HttpClient(new NativeMessageHandler());
+
+        private static readonly ApprecotRestService _instance = new ApprecotRestService();
+
+        static ApprecotRestService()
+        {
+
+        }
+
+        private ApprecotRestService()
+        {
+
+        }
+
+        public static ApprecotRestService Instance => _instance;
+
+        public async Task<string> GetAsync(string url)
+        {
+            string json;
+
+            try
+            {
+                using (var response = await Client.GetAsync(url).ConfigureAwait(false))
+                {
+                    response.EnsureSuccessStatusCode();
+                    json = await response.Content.ReadAsStringAsync();
+                }
+            }
+            catch (HttpRequestException)
+            {
+                json = string.Empty;
+            }
+            catch (WebException)
+            {
+                json = string.Empty;
+            }
+            catch (Exception)
+            {
+                json = string.Empty;
+            }
+
+            return json;
+        }
+    }
+}
