21dc757917de017297cdc054813b518934e3ccbe
Alexis Machairidis <alex.machairidis@gmail.com>
2017-03-30 10:28:19 +0300

    no internet view

--- Ts Solutions/Ts Solutions/Ts_Solutions.iOS/ViewControllers/FirstViewController/FirstViewController.cs
+++ Ts Solutions/Ts Solutions/Ts_Solutions.iOS/ViewControllers/FirstViewController/FirstViewController.cs
@@ -1,10 +1,11 @@
 ﻿using System;
-using System.Diagnostics;
 
 namespace Ts_Solutions.iOS
 {
 	public partial class FirstViewController : BaseController, IViewController
 	{
+		ReconnectingView _reconnect;
+
 		public FirstViewController() : base("FirstViewController")
 		{
 		}
@@ -34,12 +35,38 @@ namespace Ts_Solutions.iOS
 		public override void ViewDidDisappear(bool animated)
 		{
 			base.ViewDidDisappear(animated);
+			ToggleConnectionIndicator(IsOnline());
 		}
 
 		public void Reachability_ReachabilityChanged(object sender, EventArgs e)
 		{
-			Debug.WriteLine("net changed");
+			ToggleConnectionIndicator(IsOnline());
 		}
+
+
+		public void ToggleConnectionIndicator(bool internetState)
+		{
+			if (!internetState)
+			{
+				if (_reconnect == null)
+				{
+					_reconnect = ReconnectingView.Create(NavigationController.NavigationBarHidden);
+					_reconnect.Alpha = 0;
+				}
+				if (_reconnect.Alpha == 1) return;
+
+				View.Add(_reconnect);
+				_reconnect.FadeIn();
+			}
+			else
+			{
+				_reconnect?.Hide();
+				//_reconnect?.RemoveFromSuperview();
+				//_reconnect = null;
+			}
+		}
+
 	}
 }
+
 
---
+++ Ts Solutions/Ts Solutions/Ts_Solutions.iOS/Views/Reconnect/ReconnectingView.cs
@@ -0,0 +1,44 @@
+using Foundation;
+using System;
+using UIKit;
+using ObjCRuntime;
+
+namespace Ts_Solutions.iOS
+{
+    public partial class ReconnectingView : UIView
+    {
+        public ReconnectingView (IntPtr handle) : base (handle)
+        {
+        }
+
+		public ReconnectingView()
+		{
+
+		}
+
+		public static ReconnectingView Create(bool NavBarHidden)
+		{
+			var arr = NSBundle.MainBundle.LoadNib("ReconnectingView", null, null);
+			var v = Runtime.GetNSObject<ReconnectingView>(arr.ValueAt(0));
+			v.LabelInternet.Text = "Reconnecting...";//TranslationExtension.LanguageBundle.LocalizedString("reconnecting", "");
+			//v.LabelInternet.Font = UIFont.SystemFontOfSize(ConstantsiOS.FontTextSize);
+			v.LabelInternet.TextColor = UIColor.White;
+			if(NavBarHidden)
+				v.Frame = new CoreGraphics.CGRect(0, 20, UIScreen.MainScreen.Bounds.Width, 30);
+			else
+				v.Frame = new CoreGraphics.CGRect(0, 64, UIScreen.MainScreen.Bounds.Width, 30);
+			v.BackgroundColor = UIColor.DarkGray;
+
+			return v;
+		}
+
+		public void Hide()
+		{
+			UIView.Animate(
+				0.5, // duration
+				() => { Alpha = 0; },
+				() => { RemoveFromSuperview();}
+			);
+		}
+    }
+}
