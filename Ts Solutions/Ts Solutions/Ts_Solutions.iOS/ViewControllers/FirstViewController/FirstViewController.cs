da47b67ca2e3a50006d7ecdc1445f01db909194a
Alexis Machairidis <alex.machairidis@gmail.com>
2017-03-30 10:38:01 +0300

    no message

--- Ts Solutions/Ts Solutions/Ts_Solutions.iOS/ViewControllers/FirstViewController/FirstViewController.cs
+++ Ts Solutions/Ts Solutions/Ts_Solutions.iOS/ViewControllers/FirstViewController/FirstViewController.cs
@@ -1,11 +1,12 @@
 ﻿using System;
-using System.Diagnostics;
 using UIKit;
 
 namespace Ts_Solutions.iOS
 {
 	public partial class FirstViewController : BaseController, IViewController
 	{
+		ReconnectingView _reconnect;
+
 		public FirstViewController() : base("FirstViewController")
 		{
 		}
@@ -29,6 +30,7 @@ namespace Ts_Solutions.iOS
 				}
 			};
 			NavigationItem.LeftBarButtonItems = leftIcon;
+			DismissKeyboardOnBackgroundTap();
 		}
 
 		public override void ViewWillAppear(bool animate)
@@ -46,18 +48,40 @@ namespace Ts_Solutions.iOS
 		public override void ViewWillLayoutSubviews()
 		{
 			base.ViewWillLayoutSubviews();
-			ConstTopText.Constant = 16 + NavigationController.NavigationBar.Frame.Height + 20;
 		}
 
 		public override void ViewDidDisappear(bool animated)
 		{
 			base.ViewDidDisappear(animated);
+			ToggleConnectionIndicator(IsOnline());
 		}
 
 		public void Reachability_ReachabilityChanged(object sender, EventArgs e)
 		{
-			Debug.WriteLine("net changed");
+			ToggleConnectionIndicator(IsOnline());
 		}
+
+
+		public void ToggleConnectionIndicator(bool internetState)
+		{
+			if (!internetState)
+			{
+				if (_reconnect == null)
+				{
+					_reconnect = ReconnectingView.Create(NavigationController.NavigationBarHidden);
+					_reconnect.Alpha = 0;
+				}
+				if (_reconnect.Alpha == 1) return;
+
+				View.Add(_reconnect);
+				_reconnect.FadeIn();
+			}
+			else
+				_reconnect?.Hide();
+			
+		}
+
 	}
 }
+
 
