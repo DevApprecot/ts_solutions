2a62fadcb4c9862092fd1ee2b3741cf5a8087121
Alexis Machairidis <alex.machairidis@gmail.com>
2017-03-30 11:05:27 +0300

    asasad

--- Ts Solutions/Ts Solutions/Ts_Solutions/Model/ServiceResponse.cs
+++ Ts Solutions/Ts Solutions/Ts_Solutions/Model/ServiceResponse.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json;
 
 namespace Ts_Solutions.Model
 {
-    class ServiceResponse
-    {
-    }
+	public class ServiceResponse
+	{
+		[JsonProperty(PropertyName = "rs")]
+		public int StatusCode { get; set; }
+		[JsonProperty(PropertyName = "rm")]
+		public string Message { get; set; }
+		public object Data { get; set; }
+	}
 }
