4a29e9e6eb95d19a6d5c1ce3623abdd09ec9326f
Antonis Stoulos <stoulosan@gmail.com>
2017-04-12 12:14:51 +0300

    tests

---
+++ Ts Solutions/Ts_SolutionsTests/MainView.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ts_Solutions.IView;
+using Ts_Solutions.Model;
+
+namespace Ts_SolutionsTests
+{
+    public class MainView : IMainView
+    {
+        public bool Success { get; set; }
+
+        public void CallClicked(string phone)
+        {
+        }
+
+        public void CallNumber(string phone)
+        {
+        }
+
+        public void DirectionsClicked(ServicePoint point)
+        {
+        }
+
+        public void HideStatus()
+        {
+        }
+
+        public bool IsOnline()
+        {
+            return true;
+        }
+
+        public Task OnConnected()
+        {
+            return Task.Delay(0);
+        }
+
+        public void OpenDirections(ServicePoint point)
+        {
+        }
+
+        public void SetList(List<ServicePoint> points)
+        {
+        }
+
+        public void SetLoading(bool isLoading)
+        {
+        }
+
+        public void SetMarkers(List<ServicePoint> points)
+        {
+            if (points != null && points.Count > 0)
+                Success = true;
+            else
+                Success = false;
+        }
+
+        public void ShowMessage(string message)
+        {
+        }
+
+        public void ShowStatus(string status)
+        {
+        }
+        
+    }
+}
---
+++ Ts Solutions/Ts_SolutionsTests/TestClass.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ts_Solutions.Presenter;
+
+namespace Ts_SolutionsTests
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public async Task ShouldLoadServicePointsSuccessfully()
+        {
+            var view = new MainView();
+            var presenter = new MainPresenter(view);
+
+            await presenter.LoadServicePoints();
+            Assert.IsTrue(view.Success);
+        }
+        
+    }
+}
