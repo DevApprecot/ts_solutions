36956f6d840b151a669975c0867a72b71366d2d4
giorgosTh <gt@anaxoft.com>
2017-04-26 11:29:24 +0300

    no message

--- Ts Solutions/Ts Solutions/Ts_Solutions/Model/ApiUrls.cs
+++ Ts Solutions/Ts Solutions/Ts_Solutions/Model/ApiUrls.cs
@@ -4,10 +4,10 @@ namespace Ts_Solutions
 	public static class ApiUrls
 	{
 		// production branch
-		//public const string BaseAddress = "https://anaxoft.com/ts_production/api";
-
-		//development branch
 		public const string BaseAddress = "https://anaxoft.com/ts_production/api";
+
+		// development branch
+		//public const string BaseAddress = "https://anaxoft.com/ts_production/api";
 
 		public static string ServicesEndpoint = string.Empty;
 		public static string UserImagesEndpoint = string.Empty;
